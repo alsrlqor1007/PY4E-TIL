17031e94b411b162bdcdc395d1ad2a6ae4b0b211
alsrlqor <skyup9393@gmail.com>
2022-08-07 16:14:31 +0900

    Add 8-4

--- [08] 리스트/4_exercise.py
+++ [08] 리스트/4_exercise.py
@@ -10,12 +10,12 @@ for line in fileHandler:
 
 
 # Lecture reference
-# han = open('mbox-short.txt')
-#
-# for line in han:
-#     line = line.rstrip()
-#     wds = line.split()
-#
-#     if len(wds) < 3 or wds[0] != 'From' :
-#         continue
-#     print(wds[2])
+han = open('mbox-short.txt')
+
+for line in han:
+    line = line.rstrip()
+    wds = line.split()
+
+    if len(wds) < 3 or wds[0] != 'From' :
+        continue
+    print(wds[2])
