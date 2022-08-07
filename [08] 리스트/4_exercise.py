40659517d153ea5e810819d21775a9af4f7aca9c
alsrlqor <skyup9393@gmail.com>
2022-08-07 16:14:08 +0900

    Add 8-4

---
+++ [08] 리스트/4_exercise.py
@@ -0,0 +1,21 @@
+fileHandler = open('mbox-short.txt')
+
+for line in fileHandler:
+    words = line.rstrip().split()
+
+    # 조건 순서 중요
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    print(words[2])
+
+
+# Lecture reference
+# han = open('mbox-short.txt')
+#
+# for line in han:
+#     line = line.rstrip()
+#     wds = line.split()
+#
+#     if len(wds) < 3 or wds[0] != 'From' :
+#         continue
+#     print(wds[2])
